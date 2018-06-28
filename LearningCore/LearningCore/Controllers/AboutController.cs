a78acd96d556df50996b66872ebc704a1bebf459
Vipul Malhotra <vipulm124@hotmail.com>
2018-06-28 13:20:31 +0530

    basic project

---
+++ LearningCore/LearningCore/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningCore.Controller
+{
+    [Route("about")]
+    public class AboutController
+    {
+        [Route("Phone")]
+        public string Phone()
+        {
+            return "+49-333-3333333";
+        }
+        [Route("Country")]
+        public string Country()
+        {
+            return "Germany";
+        }
+    }
+}
---
+++ LearningCore/LearningCore/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using LearningCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+
+namespace LearningCore.Controller
+{
+    [Route("Home")]
+    public class HomeController : ControllerBase
+    {
+        //public ObjectResult Index()
+        //{
+        //    var employee = new Employee { ID = 1, Name = "Mark Upston" };
+        //    return new ObjectResult(employee);
+        //    //return "Hello, World! this message is from Home Controller...";
+        //}
+
+        //public ViewResult Index()
+        //{
+        //    var employee = new Employee { ID = 1, Name = "Mark Upston" };
+        //    //return View();
+        //}
+    }
+}
